8729758e1a5c6684a3f0b6a1108a1a04ce7f8c04
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-07-14 02:55:56 +0300

    lab13

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -4,7 +4,8 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
 import axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
-import { Card } from 'react-bootstrap/'
+import { Card ,Button} from 'react-bootstrap/'
+import BookFormModal from './component/BookFormModal';
 
 
 
@@ -13,16 +14,49 @@ class MyFavoriteBooks extends React.Component {
     super(props)
     this.state = {
       dataofBooks: [],
-      showData: false
+      showData: false,
+      myEmail:'',
+      showModel: false,
     }
   }
   componentDidMount = async () => {
-    const { data } = this.props.auth0;
-    let resData = await axios.get(`${process.env.REACT_APP_BOOK}book?myEmail=${data.email}`)
+    // const { data } = this.props.auth0;
+    // this.setState({
+    //   myEmail:`${data.email}`
+    // })
+    let resData = await axios.get(`${process.env.REACT_APP_BOOK}book?myEmail=${this.props.auth0.user.email}`)
 
     await this.setState({
       dataofBooks: resData.data,
       showData: true
+    })
+
+  }
+  handlerShow = async () => {
+    await this.setState({
+      showModel: true
+    })
+  }
+  handlerClose = async () => {
+    await this.setState({
+      showModel: false
+    })
+  }
+  addBook= async (e)=>{
+    e.preventDefault();
+    let bookObj={
+      email:this.state.myEmail,
+      name : e.target.bookName.value,
+      description : e.target.description.value,
+      status : e.target.state.value,
+      img : e.target.imgUrl.value,
+    }
+    let url=`${process.env.REACT_APP_BOOK}books`
+    let addResponse= await axios.post(url,bookObj)
+    
+
+    this.setState({
+      dataofBooks:addResponse.data
     })
 
   }
@@ -32,39 +66,49 @@ class MyFavoriteBooks extends React.Component {
   render() {
     return (
       <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-        <div className="bookcont">
-          {this.state.showData &&
-            this.state.dataofBooks.map(book => {
+        <>
+          <Button variant="dark" onClick={this.handlerShow}>
+            Add  Book
+          </Button>
+
+          <BookFormModal showModel={this.state.showModel} handlerClose={this.handlerClose}  addBook={this.addBook}/>
+          <h1>My Favorite Books</h1>
+          <p>
+            This is a collection of my favorite books
+          </p>
+          <div className="bookcont">
+            {this.state.showData &&
+              this.state.dataofBooks.map((book, index) => {
 
 
-              return (
-                <Card  >
+                return (
+                  <Card key={index} style={{ display: "inline-block" ,width:"200"}} >
 
-                  <Card.Body>
-                    <Card.Title>{book.name}</Card.Title>
-                    <Card.Text>
-                      {book.description}
-                    </Card.Text>
-                    <Card.Text>
-                      {book.status}
-                    </Card.Text>
-                    <Card.Img src={book.img} alt={book.name} />
-                  </Card.Body>
-                </Card>
-              )
+                    <Card.Body>
+                      <Card.Title>{book.name}</Card.Title>
+                      <Card.Text>
+                        {book.description}
+                      </Card.Text>
+                      <Card.Text>
+                        {book.status}
+                      </Card.Text>
+                      <Card.Img src={book.img} alt={book.name} />
+                    </Card.Body>
+                    <Button variant="danger" onClick={() => this.props.deleteBook(index)}>
+                      Delete
+                    </Button>
+                  </Card>
+                )
 
-            })
+              })
 
-          }
-        </div>
-
+            }
+          </div>
+        </>
       </Jumbotron>
+
     )
   }
 }
 
-export default withAuth0( MyFavoriteBooks);
+export default withAuth0(MyFavoriteBooks);
---
+++ src/component/BookFormModal.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import {Modal,Button} from 'react-bootstrap/'
+export class BookFormModal extends Component {
+
+    
+    
+    render() {
+        return (
+            <div>
+                <Modal show={this.props.showModel}>
+                    <Modal.Header>
+                        <Modal.Title>Add Book To Favorites</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <form onSubmit={this.props.addBook}>
+                            <input placeholder="Book Name" type="text" name="bookName" />
+                            <input placeholder="Book Description" type="text" name="description" />
+                            <input placeHolder="Book Image" type="text" name="imgUrl" />
+                            <select placeHolder="State" type="select" name="state">
+                                <option value="readLater">Read ....</option>
+                                <option value="topTen">Favorit Five</option>
+                                <option value="recommended">Recommended To Me</option>
+                            </select>
+                            <input type="submit" value="ADD Book" />
+                        </form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.props.handlerClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default BookFormModal
